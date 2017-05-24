035d7d88e0681c2681639a29b7dd363c7a5e455a
wwh05 <meowe94@hotmail.com>
2017-05-24 22:00:11 +0200

    new code

--- pusherteseter/Program.cs
+++ pusherteseter/Program.cs
@@ -11,6 +11,7 @@ namespace pusherteseter
         static void Main(string[] args)
         {
             Console.WriteLine("hmmer");
+            Console.WriteLine("tmmemrmerm");
         }
     }
 }
