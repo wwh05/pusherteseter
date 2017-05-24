9306f7b16c867b48b5271a1920fb90b526501611
addeponkeN <addeponken@hotmail.com>
2017-05-24 21:55:54 +0200

    Add project files.

---
+++ pusherteseter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pusherteseter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hmmer");
+
+            Console.WriteLine("HEYYER WOOWER");
+        }
+    }
+}
